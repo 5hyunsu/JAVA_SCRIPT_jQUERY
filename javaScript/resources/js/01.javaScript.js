5a5d130e18ab5913c95e4a315d396ec7179259c5
5hyunsu <water.steam.cloud.rain@gmail.com>
2023-11-30 17:26:11 +0900

    Day01_자바콘솔

---
+++ javaScript/resources/js/01.javaScript.js
@@ -0,0 +1,8 @@
+function test() {
+    window.alert('별도로 작성된 js파일');
+    //별도의 스타일 넣어주면 링크 걸어줘야 한다.
+    //자바 링크 걸어주기  
+    //<script src="resources/js/01.javaScript.js" rel="javascript"></script>
+    //이벤트명을 알아야 무엇을 실행할지 코드를 넣어준다. 
+    
+}
